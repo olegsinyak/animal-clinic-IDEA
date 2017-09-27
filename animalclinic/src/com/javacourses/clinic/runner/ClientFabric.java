62c85150004ef83329f5eec105451c14296831af
ioleg <ioleg100500@gmail.com>
2017-09-27 21:18:51 +0300

    Refactor ClientFabric

--- animalclinic/src/com/javacourses/clinic/runner/ClientFabric.java
+++ animalclinic/src/com/javacourses/clinic/runner/ClientFabric.java
@@ -8,18 +8,17 @@ import java.util.Random;
  * Fabric creates random clients.
  */
 public class ClientFabric {
-    private static final String[] CLIEND_IDS = {"Abrham", "Polly", "John", "Arnold", "Bill", "Smith", "Greg"};
+    private static final String[] CLIENT_IDS = {"Abrham", "Polly", "John", "Arnold", "Bill", "Smith", "Greg"};
     private static final String[] PET_NAMES = {"Fill", "Varick", "Bob", "Buks", "Flops", "Admiral", "General", "Swift"};
-
+    private static Random random = new Random();
 
     /**
      * Creates new client with random id and pet.
      * @return random client.
      */
     public static Client next() {
-        Random r = new Random(47);
-        String id = CLIEND_IDS[r.nextInt(CLIEND_IDS.length)];
-        String pet = PET_NAMES[r.nextInt(PET_NAMES.length)];
+        String id = CLIENT_IDS[random.nextInt(CLIENT_IDS.length)];
+        String pet = PET_NAMES[random.nextInt(PET_NAMES.length)];
         return new Client(id, pet);
     }
 
